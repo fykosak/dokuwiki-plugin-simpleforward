fb94eb1ec68fc5f261df0d781d3a0ae5adf77116
Michal Koutný <xm.koutny@gmail.com>
2014-02-22 11:22:40 +0100

    initialized from plugin wizard

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the simpleforward plugin
+ *
+ * @author Michal Koutný <michal@fykos.cz>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the simpleforward plugin
+ *
+ * @author Michal Koutný <michal@fykos.cz>
+ */
+
+
+//$meta['fixme'] = array('string');
+
